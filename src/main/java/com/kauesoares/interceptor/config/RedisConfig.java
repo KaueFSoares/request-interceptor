2850f3e93ffe8a77d72374ab39a4a15f9a3a3fb5
Kauê Soares <kaue222120@gmail.com>
2025-03-02 22:19:45 -0300

    fix: redis config

--- src/main/java/com/kauesoares/interceptor/config/RedisConfig.java
+++ src/main/java/com/kauesoares/interceptor/config/RedisConfig.java
@@ -1,53 +1,40 @@
 package com.kauesoares.interceptor.config;
 
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SocketOptions;
-import io.lettuce.core.TimeoutOptions;
-import io.lettuce.core.resource.ClientResources;
-import io.lettuce.core.resource.DefaultClientResources;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean(destroyMethod = "shutdown")
-    public ClientResources clientResources() {
-        return DefaultClientResources.builder()
-                .ioThreadPoolSize(10)
-                .computationThreadPoolSize(10)
-                .build();
-    }
-
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory(ClientResources clientResources) {
-        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration("localhost", 6379);
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration("localhost", 6379);
+
+        GenericObjectPoolConfig<Object> poolConfig = new GenericObjectPoolConfig<>();
+        poolConfig.setMaxTotal(10);
+        poolConfig.setMaxIdle(10);
+        poolConfig.setMinIdle(0);
 
         LettucePoolingClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
-                .clientResources(clientResources)
-                .clientOptions(ClientOptions.builder()
-                        .socketOptions(SocketOptions.builder()
-                                .connectTimeout(Duration.ofSeconds(10))
-                                .build())
-                        .timeoutOptions(TimeoutOptions.builder()
-                                .fixedTimeout(Duration.ofSeconds(5))
-                                .build())
-                        .build())
+                .commandTimeout(Duration.ofSeconds(2))
+                .poolConfig(poolConfig)
                 .build();
 
-        return new LettuceConnectionFactory(redisConfig, clientConfig);
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory(clientResources()));
+        template.setConnectionFactory(connectionFactory);
         return template;
     }
 }
